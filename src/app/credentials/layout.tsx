50cd96207c413284a94263d9fcbc0f6ccd519935
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-04-08 11:16:39 +0530

    update

--- src/app/credentials/layout.tsx
+++ src/app/credentials/layout.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
 import Header from './_components/header'
 
-const layout = ({ children } : { children: React.ReactNode }) => {
+const layout = async ({ children } : { children: React.ReactNode }) => {
+    const supabase = createClient()
+
+    const { data: { user } } = await supabase.auth.getUser()
+
+    if(!user) redirect("/login")
+
   return (
     <div>
         <Header />
--- src/app/credentials/page.tsx
+++ src/app/credentials/page.tsx
@@ -1,16 +1,8 @@
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {}
 
 const Credentials = async (props: Props) => {
-
-    const supabase = createClient()
-
-    const { data: { user } } = await supabase.auth.getUser()
-
-    if(!user) redirect("/login")
 
   return (
     <div>Credentials</div>
