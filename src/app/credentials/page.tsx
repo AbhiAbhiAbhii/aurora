670e7342961fe0a756998b203b6cc0dca52e4f38
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-04-17 14:12:40 +0530

    testing

--- src/app/credentials/page.tsx
+++ src/app/credentials/page.tsx
@@ -9,9 +9,9 @@ const Credentials = async (props: Props) => {
 
   const supabase = createClient()
 
-  const { data: { user } } = await supabase.auth.getUser()
+  // const { data: { user } } = await supabase.auth.getUser()
 
-  if(!user) redirect("/login")
+  // if(!user) redirect("/login")
 
   return (
     <div className='flex flex-col items-start px-10 justify-center mt-12'>
